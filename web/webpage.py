d522c4c723f6c20a7389d8fe9041f1a4b79b0fd9
nmondora <nmondora@purdue.edu>
2023-12-19 14:55:00 -0500

    added POST/GET

--- web/webpage.py
+++ web/webpage.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
@@ -8,9 +8,39 @@ def home():
     return render_template("start.html")
 
 # Defining the fanno flow page
-@app.route("/fanno")
+@app.route("/fanno", methods=["POST", "GET"])
 def fanno():
-    return render_template("fanno.html")
+    if request.method == "POST":
+        fluid = request.form["fluid"]
+        gamma = request.form["gamma"]
+        tubeDiam = request.form["tubeDiam"]
+        tubeLen = request.form["tubeLen"]
+        frictionCoeff = request.form["frictionCoeff"]
+        standardVolFlow = request.form["standardVolFlow"]
+        massFlow = request.form["massFlow"]
+        upstreamMach = request.form["upstreamMach"]
+        upstreamVel = request.form["upstreamVel"]
+        upstreamPress = request.form["upstreamPress"]
+        upstreamTemp = request.form["upstreamTemp"]
+
+        return render_template("fanno.html", fluid=fluid, gamma=gamma, tubeDiam=tubeDiam,
+                               tubeLen=tubeLen, frictionCoeff=frictionCoeff, standardVolFlow=standardVolFlow,
+                               massFlow=massFlow, upstreamMach=upstreamMach, upstreamVel=upstreamVel,
+                               upstreamPress=upstreamPress, upstreamTemp=upstreamTemp)
+    else:
+        fluid = ""
+        gamma = ""
+        tubeDiam = ""
+        tubeLen = ""
+        frictionCoeff = ""
+        standardVolFlow = ""
+        massFlow = ""
+        upstreamMach = ""
+        upstreamVel = ""
+        upstreamPress = ""
+        upstreamTemp = ""
+
+        return render_template("fanno.html")
 
 # Defining the rayleigh flow page
 @app.route("/rayleigh")
